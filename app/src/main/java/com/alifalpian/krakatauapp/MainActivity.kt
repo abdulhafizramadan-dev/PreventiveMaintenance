78ad32bc35d3b86ef25a52f18a70a8b355691933
Abdul Hafiz Ramadan <abdulhafizramadan.dev@gmail.com>
2023-09-11 00:31:39 +0700

    | App Flow

--- app/src/main/java/com/alifalpian/krakatauapp/MainActivity.kt
+++ app/src/main/java/com/alifalpian/krakatauapp/MainActivity.kt
@@ -3,15 +3,18 @@ package com.alifalpian.krakatauapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.alifalpian.krakatauapp.presentation.NavGraphs
 import com.alifalpian.krakatauapp.ui.theme.PreventiveMaintenanceTheme
+import com.ramcosta.composedestinations.DestinationsNavHost
 
+@ExperimentalFoundationApi
+@ExperimentalMaterial3Api
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,25 +25,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    DestinationsNavHost(navGraph = NavGraphs.root)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PreventiveMaintenanceTheme {
-        Greeting("Android")
     }
 }
